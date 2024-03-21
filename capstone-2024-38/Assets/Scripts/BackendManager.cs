6d4b8d1aff6a33654ae588509d4a3961b348eb95
biyodde <billjhkim@kookmin.ac.kr>
2024-03-21 17:31:33 +0900

    [TEST] 회원가입, 로그인, 닉네임 변경 테스트 코드 추가

---
+++ capstone-2024-38/Assets/Scripts/BackendManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+using System.Threading.Tasks;
+
+public class BackendManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 뒤끝 초기화
+        var bro = Backend.Initialize(true);
+        
+        // 뒤끝 초기화에 대한 응답값
+        if(bro.IsSuccess()) 
+        {
+            Debug.Log("초기화 성공 : " + bro); // 성공일 경우 statusCode 204 Success
+        } else 
+        {
+            Debug.LogError("초기화 실패 : " + bro); // 실패일 경우 statusCode 400대 에러 발생
+        }
+
+        Test();
+    }
+
+    async void Test()
+    {
+        await Task.Run(() =>
+        {
+            //BackendLogin.Instance.CustomSignUp("user1", "1234");
+            BackendLogin.Instance.CustomLogin("user1", "1234");
+            BackendLogin.Instance.UpdateNickname("원하는 이름");
+            Debug.Log("테스트를 종료합니다");
+        });
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
