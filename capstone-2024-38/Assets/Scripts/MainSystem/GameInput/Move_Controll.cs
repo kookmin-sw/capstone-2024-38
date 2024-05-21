1a91ecd136132f0f4a7964c95250cf8e929b6379
SongTaeHo1 <ehgf7569@gmail.com>
2024-05-22 06:18:13 +0900

    [FIX] 플레이어 애니메이션 오류 수정

--- capstone-2024-38/Assets/Scripts/MainSystem/GameInput/Move_Controll.cs
+++ capstone-2024-38/Assets/Scripts/MainSystem/GameInput/Move_Controll.cs
@@ -9,7 +9,6 @@ public class move_Controll : MonoBehaviour
     public float jumpForce = 12.0f;
     public CinemachineFreeLook freeLookCamera;
 
-
     Rigidbody rigid;
     Vector3 moveVector;
     Animator player_animation;
@@ -18,8 +17,9 @@ public class move_Controll : MonoBehaviour
     public float groundCheckDistance = 1.1f;
     private bool isGrounded = false;
     private bool isMoving = false;
-    public float pushForce = 10.0f; 
+    public float pushForce = 10.0f;
 
+    private bool isJumping = false;
 
     void Start()
     {
@@ -38,17 +38,17 @@ public class move_Controll : MonoBehaviour
 
         if (x != 0 || z != 0)
         {
-            float angle = Mathf.Atan2(z, x) * Mathf.Rad2Deg; 
+            float angle = Mathf.Atan2(z, x) * Mathf.Rad2Deg;
             angle += Fx;
             if (angle < 0f)
             {
                 angle += 360f;
             }
 
-            
             Vector3 moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
-            
-            if (Input.GetKey(KeyCode.LeftShift)){
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
                 moveVector = moveDirection * (moveSpeed * 1.5f);
             }
             else
@@ -79,24 +79,33 @@ public class move_Controll : MonoBehaviour
             player_animation.SetBool("IsRun", true);
         }
 
-
         isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer);
 
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             Jump();
-
         }
 
         if (!isGrounded)
         {
-            player_animation.SetBool("IsMove", false);
-            player_animation.SetBool("IsRun", false);
-            player_animation.SetTrigger("Jump");
+            if (!isJumping) 
+            {
+                isJumping = true;
+                player_animation.SetBool("IsMove", false);
+                player_animation.SetBool("IsRun", false);
+                player_animation.SetTrigger("Jump");
+                StartCoroutine(PauseJumpAnimation());
+            }
         }
         else
         {
-            player_animation.ResetTrigger("Jump");
+            if (isJumping)
+            {
+                isJumping = false;
+                player_animation.ResetTrigger("Jump");
+                player_animation.speed = 1f; 
+            }
+
             if (isMoving)
             {
                 player_animation.SetBool("IsMove", true);
@@ -117,7 +126,6 @@ public class move_Controll : MonoBehaviour
                 player_animation.SetBool("IsMove", true);
                 player_animation.SetBool("IsRun", false);
             }
-
         }
 
         if (Input.GetKey(KeyCode.E))
@@ -125,10 +133,7 @@ public class move_Controll : MonoBehaviour
             player_animation.SetTrigger("Push");
             PushPlayer();
         }
-
-
     }
-
 
     void FixedUpdate()
     {
@@ -145,13 +150,11 @@ public class move_Controll : MonoBehaviour
     private void Jump()
     {
         rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-
     }
 
     private void PushPlayer()
     {
-        
-        Vector3 pushDirection = transform.forward; 
+        Vector3 pushDirection = transform.forward;
         Vector3 pushPosition = transform.position + transform.forward * 1.5f;
         Collider[] colliders = Physics.OverlapSphere(pushPosition, 0.5f);
 
@@ -165,4 +168,12 @@ public class move_Controll : MonoBehaviour
         }
     }
 
+    private IEnumerator PauseJumpAnimation()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (!isGrounded) 
+        {
+            player_animation.speed = 0f; 
+        }
+    }
 }
